8cc4a8b8a0ecc34bafc0aba3094b235259e08b13
andrewpratt64 <adpratt@yahoo.com>
2021-06-27 01:31:42 -0400

    Fixed typo

--- bat/deploy_all_single.bat
+++ bat/deploy_all_single.bat
@@ -3,7 +3,7 @@
 @echo off
 
 :: Run clean.js to delete old rpkg files
-node %~dp0\..\src\gen.js %~dp0\..\config.json false true
+node %~dp0\..\src\clean.js %~dp0\..\config.json false true
 
 :: Run deploy.js to copy rpkg to Hitman 3 directory
 node %~dp0\..\src\deploy.js %~dp0\..\config.json test.entity.rpkg
